5d79c8ed024e232065493f9cd6cdc29218a1e779
Naff Mwaura <naftali795@gmail.com>
2025-06-03 03:01:42 +0300

    Favicon image

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: './',
 })
+
+/*
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 👈 Add this line if deploying to Netlify
+})
+
+*/
