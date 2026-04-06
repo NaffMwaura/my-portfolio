de3abc3ed8a1c28b4165862d4674a6f7ff66decd
Naff Mwaura <naftali795@gmail.com>
2026-04-06 13:28:11 +0300

    Force Offline Architecture:PWA

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { registerSW } from 'virtual:pwa-register' // This is the "magic" import
+
+// This function registers the service worker 
+// and tells it how to handle updates
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('New version available. Update now?')) {
+      updateSW(true)
+    }
+  },
+  onOfflineReady() {
+    console.log('Portfolio is ready for offline use!')
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
