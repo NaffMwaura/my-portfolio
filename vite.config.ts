ce43ed46d00bbc89aee24beda79cde33eca0a6cb
Naff Mwaura <naftali795@gmail.com>
2026-04-06 13:12:10 +0300

    Offline Architecture:PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,44 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: './',
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        // This ensures all your images in assets are cached for offline use
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,jpg}']
+      },
+      manifest: {
+        name: 'Naff Mwaura Portfolio',
+        short_name: 'NaffPort',
+        description: 'Software Developer & AI/ML Engineer Portfolio',
+        theme_color: '#0b0c2a',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
 
-/*
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: './', // 👈 Add this line if deploying to Netlify
-})
 
-*/
+
+
+
+
+//  plugins: [react(),],
